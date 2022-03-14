79b5bb33a4c6a61948b14592b1eb9c9950c57506
Carlos Roberto De Rolt Junior <carlosroltjr@gmail.com>
2022-03-14 01:11:50 -0300

    fixing class name

--- src/classes/InstanceChallenge.java
+++ src/classes/InstanceChallenge.java
@@ -1,12 +1,12 @@
 package classes;
 
-public class TraumaChallenge {
+public class InstanceChallenge {
 	int number1 = 3;
 	static int number2 = 5;
 	
 	public static void main(String[] args) {
 		System.out.println(number2);
-		TraumaChallenge c = new TraumaChallenge();
+		InstanceChallenge c = new InstanceChallenge();
 		System.out.println(c.number1);
 	}
 }
